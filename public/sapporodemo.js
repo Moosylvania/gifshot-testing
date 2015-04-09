ab08b72ed175372d454005a116f94131c818738e
obj63mc <obj63mc@gmail.com>
2015-04-09 10:25:45 -0500

    update for loopability

--- public/sapporodemo.js
+++ public/sapporodemo.js
@@ -160,12 +160,39 @@
                             $("#progressbar").progressbar("option", "value", false);
                         }
                     }
-                }, function(obj) {                    
+                }, function(obj) {
                     $("#progressbar").hide();
                     var img = document.createElement('img');
                     img.src = obj.image;
                     $('.preview').empty().append(img);
                     $('.preview').show();
+                    $(img).css('cursor', 'pointer');
+                    img.onclick = function() {
+                        // atob to base64_decode the data-URI
+                        var image_data = atob(img.src.split(',')[1]);
+                        // Use typed arrays to convert the binary data to a Blob
+                        var arraybuffer = new ArrayBuffer(image_data.length);
+                        var view = new Uint8Array(arraybuffer);
+                        for (var i=0; i<image_data.length; i++) {
+                            view[i] = image_data.charCodeAt(i) & 0xff;
+                        }
+                        try {
+                            // This is the recommended method:
+                            var blob = new Blob([arraybuffer], {type: 'application/octet-stream'});
+                        } catch (e) {
+                            // The BlobBuilder API has been deprecated in favour of Blob, but older
+                            // browsers don't know about the Blob constructor
+                            // IE10 also supports BlobBuilder, but since the `Blob` constructor
+                            //  also works, there's no need to add `MSBlobBuilder`.
+                            var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder);
+                            bb.append(arraybuffer);
+                            var blob = bb.getBlob('application/octet-stream'); // <-- Here's the Blob
+                        }
+
+                        // Use the URL object to create a temporary URL
+                        var url = (window.webkitURL || window.URL).createObjectURL(blob);
+                        location.href = url; // <-- Download!
+                    };
                 });
 
             });
